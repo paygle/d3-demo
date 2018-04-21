870e0939757cb81794ccda76f7a1930190dd1f96
paygle <dodar@qq.com>
2018-04-22 01:35:38 +0800

    add examples

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,15 @@
 // 服务器主机端口配置
+let n = require('os').networkInterfaces();
+let myIp = function () {
+  for(let k in n) {
+    let inter = n[k];
+    for(let j in inter)
+      if(inter[j].family === 'IPv4' && !inter[j].internal)
+        return inter[j].address;
+  }
+};
+
 module.exports = {
-  host: 'http://localhost',
+  host: 'http://' + myIp(),
   port: 3737
 }
